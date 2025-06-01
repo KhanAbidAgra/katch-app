cf0b3afb9c8ebce5381562a83938e7fded6798b4
Abid Khan <abidkh.ak64@gmail>
2025-06-01 15:54:37 +0530

    overwrite get party detail

--- katch/katch/api.py
+++ katch/katch/api.py
@@ -1,16 +1,32 @@
-from frappe import _
 import frappe
-from erpnext.accounts.party import get_party_details, set_address_details
+from frappe import _
+from erpnext.accounts.party import get_party_account
 
 @frappe.whitelist()
 def custom_get_party_details_api(party_type, party, doctype, company):
-    print("Testing")
-    party_details = get_party_details(
-        party=party,
-        party_type=party_type,
-        company=company,
-        doctype=doctype,
+    # Build party details safely
+    party_details = frappe._dict()
+
+    party_details.account = get_party_account(party_type, party, company)
+    
+    # Fetch currency
+    party_doc = frappe.get_doc(party_type, party)
+    party_details.currency = party_doc.default_currency or frappe.get_cached_value("Company", company, "default_currency")
+
+    # Optional: fetch and set address manually (excluding gstin)
+    billing_address = frappe.db.get_value(
+        "Dynamic Link",
+        {
+            "link_doctype": party_type,
+            "link_name": party,
+            "parenttype": "Address"
+        },
+        "parent"
     )
 
+    if billing_address:
+        address_doc = frappe.get_doc("Address", billing_address)
+        party_details.address_display = address_doc.get("address_line1") or ""
+        party_details.city = address_doc.get("city")
+
     return party_details
-    # set_address_details
